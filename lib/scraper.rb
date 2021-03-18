d38cfbaaeb8ad3a4c0d4f96a5042cfee439bd2f2
SoniaDumitru <sonya_dumitru@yahoo.com>
2021-03-17 22:42:09 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+# doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css(".buttonBlue-3yHkWz").text
+
+puts doc
